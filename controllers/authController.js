6ccef6b9a39a387d65271c66d0c0d89d2ebb04f2
Mohan <veerubhotla.mohan@gmail.com>
2022-11-17 18:14:24 +0530

    Authentication for backend finished

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,5 @@
+const jwt = require("jsonwebtoken");
+const sendgridmail = require("@sendgrid/mail");
 const User = require("../models/User");
 
 exports.signup = async (req, res, next) => {
@@ -10,15 +12,99 @@ exports.signup = async (req, res, next) => {
       email,
       password,
     };
-    await User.create(newUser);
+    const token = jwt.sign(
+      {
+        name,
+        email,
+        password,
+      },
+      process.env.JWT_ACCOUNT_ACTIVATION,
+      { expiresIn: "10m" }
+    );
+    sendgridmail.setApiKey(process.env.SENDGRID_API_KEY);
+    const emailData = {
+      from: process.env.EMAIL_FROM,
+      to: email,
+      subject: `Account activation link`,
+      html: `
+      <h1>Please use the following link to activate your account </h1>
+      <p>${process.env.CLIENT_URL}/auth/activate/${token}</p>
+      <hr />
+      <p>This email contains sensitive information</p>
+      <p>${process.env.CLIENT_URL}</p>
+      `,
+    };
+    await sendgridmail.send(emailData);
     res.status(200).json({
       status: "success",
-      message: "User created",
+      message: `Activation link sent to ${email}`,
     });
   } catch (error) {
     res.status(400).json({
       status: "fail",
+      error: error,
       message: error.message,
+    });
+  }
+};
+
+exports.activateAccount = async (req, res) => {
+  const { token } = req.body;
+  try {
+    const { name, email, password } = jwt.verify(
+      token,
+      process.env.JWT_ACCOUNT_ACTIVATION
+    );
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      throw new Error("User exists with given email");
+    }
+    await User.create({ name, email, password });
+    res.status(200).json({
+      status: "success",
+      message: "Account creation successful",
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      error: error.message,
+    });
+  }
+};
+
+exports.signin = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (user) {
+      const correctCredentials = user.authenticate(password);
+      if (!correctCredentials) {
+        res.status(401).json({
+          status: "fail",
+          message: "Bad credentials",
+        });
+      }
+    } else {
+      res.status(401).json({
+        status: "fail",
+        message: "Bad credentials",
+      });
+    }
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    res.status(200).json({
+      status: "success",
+      token,
+      name: user.name,
+      email,
+      role: user.role,
+      _id: user._id,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      error: error.message,
     });
   }
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,15 @@
 const express = require("express");
-const { signup } = require("../controllers/authController");
+const {
+  signup,
+  activateAccount,
+  signin,
+} = require("../controllers/authController");
 const { userSignupValidator } = require("../validators/authValidator");
 const { runValidation } = require("../validators/indexValidator");
 const authRouter = express.Router();
 
 authRouter.post("/signup", userSignupValidator, runValidation, signup);
+authRouter.post("/signin", signin);
+authRouter.post("/activateAccount", activateAccount);
 
 module.exports = authRouter;
